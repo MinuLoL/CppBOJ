fe3da8960481443c20099501ab769b6c4d8c8511
MinuLoL <alsdn4837@naver.com>
2020-07-27 22:34:46 +0900

    add 1로만들기 solved

---
+++ DP/1로 만들기.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int d[1000001];
+
+int dp(int x)
+{
+	if(x==1)return 0;
+	if(d[x]!=0)return d[x];
+	
+	if(x%3==0&&x%2==0)
+	{
+		int a,b,c;
+		a=(dp(x/3)+1);
+		b=(dp(x/2)+1);
+		c=(dp(x-1)+1);
+		
+		vector<int> v;
+		v.push_back(a);
+		v.push_back(b);
+		v.push_back(c);
+		sort(v.begin(),v.end());
+		return d[x]=v[0];
+		
+	}
+	else if(x%3==0)
+	{
+		if(dp(x/3)+1>dp(x-1)+1)
+		{
+			return d[x]=(dp(x-1)+1);
+		}
+		else
+			return d[x]=(dp(x/3)+1);
+	}
+	else if(x%2==0)
+	{
+		if(dp(x/2)+1>dp(x-1)+1)
+		{
+			return d[x]=(dp(x-1)+1);
+		}
+		else
+			return d[x]=(dp(x/2)+1);
+	}
+	else if(x%3!=0&&x%2!=0)
+	{
+		return d[x]=dp(x-1)+1;	
+	}	
+}
+	
+//	if(x%3==2&&x%2!=0)return dp(x-1)+1;
+//	if(x%3==2&&x%2==0)return dp(x/2)+1; 
+//	if(x%3==1)return dp(x-1)+1; 
+//	if(x%3==0)return dp(x/3)+1;//3ÀÇ n½Â
+
+
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<dp(n);
+}
