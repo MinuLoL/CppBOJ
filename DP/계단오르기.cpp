7941fab0cfea79406f436181fc6d5bc0c74bd848
KKMinu <58761098+MinuLoL@users.noreply.github.com>
2020-07-30 10:28:35 +0900

    시간초과->반례 오류

--- DP/계단오르기.cpp
+++ DP/계단오르기.cpp
@@ -1,8 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n;
-int step[301]={0,};
-int d[10000];
+int step[300]={0,};
 //int d[10000];
 //
 //int dp(int x)
@@ -21,33 +20,44 @@ int dp(int x)
 {
 	if(x==1)
 	{
-		return step[n];
+		return step[n-1];
 	}
-	if(x==2)//이 부분 수정필요
+	if(n>2&&x==2)//이 부분 수정필요
 	{
-		return step[n];
+		return step[n-1];
+	}
+	if(n==2&&x==2)
+	{
+		return step[n-2]+step[n-1];
 	}
 	if(x==3)
 	{
-		if((step[n-2]+step[n])>(step[n-1]+step[n]))
-			return step[n-2]+step[n];
+		if((step[n-3]+step[n-1])>(step[n-2]+step[n-1]))
+			return step[n-3]+step[n-1];
 		else
-			return step[n-1]+step[n];
+			return step[n-2]+step[n-1];
 	}
-	if(x==4)
+	if(n==4&&x==4)
+	{
+		int a,b,c;
+		a=step[n-4]+step[n-2]+step[n-1];
+		b=step[n-3]+step[n-1];
+		c=step[n-4]+step[n-3]+step[n-1];
+		return max({a,b,c});
+	}
+	if(n>4&&x==4)
 	{
 		int a,b;
-		a=step[n-3]+step[n-1]+step[n];
-		b=step[n-2]+step[n];
+		a=step[n-4]+step[n-2]+step[n-1];
+		b=step[n-3]+step[n-1];
 		return max({a,b});
 	}
-	if(d[x]!=0)return d[x];
-	else// 이부분수정필요
+	else
 	{
 		int e,f,g;
-		e=step[n-x+2]+dp(x-2);
-		f=step[n-x+1]+step[n-x+3]+dp(x-3);
-		g=step[n-x+1]+step[n-x+2]+step[n-x+4]+dp(x-4);
+		e=step[n-x+1]+dp(x-2);
+		f=step[n-x]+step[n-x+2]+dp(x-3);
+		g=step[n-x]+step[n-x+1]+step[n-x+3]+dp(x-4);
 		return max({e,f,g});
 	}
 	
@@ -60,7 +70,7 @@ int main()
 
 	int num;
 	cin>>n;
-	for(int i=1;i<=n;++i)
+	for(int i=0;i<n;++i)
 	{
 		cin>>num;
 		step[i]=num;
