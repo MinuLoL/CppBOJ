2e0891a6dfca2813f9ad7905e18327a70e004cd8
KKMinu <58761098+MinuLoL@users.noreply.github.com>
2020-07-30 16:39:47 +0900

    Update 1로 만들기.cpp

--- DP/1로 만들기.cpp
+++ DP/1로 만들기.cpp
@@ -1,76 +1,62 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n;
-int step[301]={0,};
-int d[301];
+
+int d[1000001];
+
 int dp(int x)
 {
-	
-	if(x<1)return 0;
+	if(x==1)return 0;
 	if(d[x]!=0)return d[x];
-	else if(x==1)
-	{
-		return d[x]=step[n];
-	}
-	else if(n>2&&x==2)
-	{
-		return d[x]=step[n];
-	}
-	else if(n==2&&x==2)
-	{
-		return d[x]=step[n-1]+step[n];
-	}
-	else if(x==3)
-	{
-		int a,b;
-		a=step[n-2]+step[n];
-		b=step[n-1]+step[n];
-		return d[x]=max(a,b);
-	}
-	else if(n>4&&x==4)
-	{
-		int a,b;
-		a=step[n-2]+step[n];
-		b=step[n-3]+step[n-1]+step[n];
-
-		return d[x]=max(a,b);
-	}
-	else if(n==4&&x==4)
+	
+	if(x%3==0&&x%2==0)
 	{
 		int a,b,c;
-		a=step[n-2]+step[n];
-		b=step[n-3]+step[n-1]+step[n];
-		c=step[n-3]+step[n-2]+step[n];
-		return d[x]=max({a,b,c});
+		a=(dp(x/3)+1);
+		b=(dp(x/2)+1);
+		c=(dp(x-1)+1);
+		
+		vector<int> v;
+		v.push_back(a);
+		v.push_back(b);
+		v.push_back(c);
+		sort(v.begin(),v.end());
+		return d[x]=v[0];
+		
 	}
-	else if(x>=5&&x==n)
+	else if(x%3==0)
 	{
-		int e,f,g;
-		e=step[n-x+2]+dp(x-2);
-		f=step[n-x+1]+step[n-x+3]+dp(x-3);
-		g=step[n-x+1]+step[n-x+2]+step[n-x+4]+dp(x-4);
-		return d[x]=max({e,f,g});
+		if(dp(x/3)+1>dp(x-1)+1)
+		{
+			return d[x]=(dp(x-1)+1);
+		}
+		else
+			return d[x]=(dp(x/3)+1);
 	}
-	else if(x>=5&&x<n)
+	else if(x%2==0)
 	{
-		int e,f;
-		e=step[n-x+2]+dp(x-2);
-		f=step[n-x+1]+step[n-x+3]+dp(x-3);
-
-		return d[x]=max(e,f);
+		if(dp(x/2)+1>dp(x-1)+1)
+		{
+			return d[x]=(dp(x-1)+1);
+		}
+		else
+			return d[x]=(dp(x/2)+1);
 	}
+	else if(x%3!=0&&x%2!=0)
+	{
+		return d[x]=dp(x-1)+1;	
+	}	
 }
+	
+//	if(x%3==2&&x%2!=0)return dp(x-1)+1;
+//	if(x%3==2&&x%2==0)return dp(x/2)+1; 
+//	if(x%3==1)return dp(x-1)+1; 
+//	if(x%3==0)return dp(x/3)+1;//3의 n승
+
+
+
 int main()
 {
-
-	int num;
+	int n;
 	cin>>n;
-	for(int i=1;i<=n;++i)
-	{
-		cin>>num;
-		step[i]=num;
-	}
 	cout<<dp(n);
-	
-	
 }
