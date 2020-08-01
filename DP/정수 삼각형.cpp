ede94d67a900131ac9f9808ee1c6d8b6abb385d4
KKMinu <58761098+MinuLoL@users.noreply.github.com>
2020-08-02 00:18:36 +0900

    Update 정수 삼각형.cpp

--- DP/정수 삼각형.cpp
+++ DP/정수 삼각형.cpp
@@ -1,23 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int N;
 int arr[500][500];
-int d[500][100000000000];
-vector<int> v;
+int d[500][500];
+int dparr[500];
 
 int dp(int x)
 {
-	if(x==1)return arr[0][0];
+	if(x==1)
+	{
+		d[0][0]=arr[0][0];
+		return d[0][0];
+	}
+	if(dparr[x]!=0)return dparr[x];
 	else
 	{
-		
+		for(int i=0;i<x;++i)
+		{
+			if(i==0)
+			{
+				d[x-1][i]=d[x-2][0]+arr[x-1][i]; //d[][]가 저장안
+			}
+			else if(i==x-1)
+			{
+				d[x-1][i]=d[x-2][x-1]+arr[x-1][i];
+			}
+			else
+			{
+				int a,b;
+				a=arr[x-1][i]+d[x-2][i-1];
+				b=arr[x-1][i]+d[x-2][i];
+				d[x-1][i]=min(a,b);
+			}
+		}
+		int maxnum=0;
+		for(int i=0;i<x;++i)
+		{
+			if(maxnum<d[x-1][i])
+				maxnum=d[x-1][i];
+		}
+		return dparr[x]=maxnum;
 	}
+	
 	
 }
 
 
-
-int main(void)
+int main()
 {
 	cin>>N;
 	for(int i=0;i<N;++i)
@@ -27,7 +57,9 @@ int main(void)
 			cin>>arr[i][j];
 		}
 	}
-
 	cout<<dp(N);
+	
+	
+	
 	
 }
