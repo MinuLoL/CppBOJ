b4f512609f67e7a4a76762e722fe9f65d72d83eb
KKMinu <58761098+MinuLoL@users.noreply.github.com>
2020-08-03 10:14:20 +0900

    Update 정수 삼각형.cpp

--- DP/정수 삼각형.cpp
+++ DP/정수 삼각형.cpp
@@ -4,37 +4,31 @@ using namespace std;
 int N;
 int arr[500][500];
 int d[500][500];
-
+vector<int> v;
 
 int dp(int x,int y)//x는 갯수(N) y는 열  
 {
-	if(x==1)return arr[0][0];
 	if(d[x][y]!=0)return d[x][y];
+	if(x==0)return arr[0][0];
 	else
 	{
 		if(y==0)
 		{
-			return d[x-1][y]=dp(x-1,0)+arr[x-1][y]; //d[][]가 저장안
+			return d[x][y]=dp(x-1,0)+arr[x][y]; //d[][]가 저장안
 		}
-		else if(y==x-1)
+		else if(y==x)
 		{
-			return d[x-1][y]=dp((x-1),(x-1))+arr[x-1][y];
+			return d[x][y]=dp((x-1),(x-1))+arr[x][y];
 		}
 		else
 		{
 			int a,b;
-			a=arr[x-1][y]+dp((x-1),(y-1));
-			b=arr[x-1][y]+dp((x-1),(y));
-			return d[x-1][y]=min(a,b);
+			a=arr[x][y]+dp((x-1),(y-1));
+			b=arr[x][y]+dp((x-1),(y));
+			return d[x][y]=max(a,b);
 		}
 	}
-//		int maxnum=0;
-//		for(int i=0;i<x;++i)
-//		{
-//			if(maxnum<d[x-1][i])
-//				maxnum=d[x-1][i];
-//		}
-//		return dparr[x]=maxnum;
+
 }
 
 
@@ -48,17 +42,21 @@ int main()
 			cin>>arr[i][j];
 		}
 	}
-	dp(1,0);
-	dp(2,0);
-	dp(2,1);
-	dp(3,0);
-	dp(3,1);
-	dp(3,2);
 
 
-	
-	cout<<dp(3,1);
-	
-	
+	for(int i=0;i<N;++i)
+	{
+		for(int j=0;j<=i;++j)
+		{
+			dp(i,j);
+		}
+	}
+	for(int i=0;i<N;++i)
+	{
+		v.push_back(dp(N-1,i));
+	}
+	vector<int>::iterator iter;
+	iter=max_element(v.begin(),v.end());
+	cout<<*iter<<endl;
 	
 }
