f49a73b748f98a5324603133b4930ac4f4a31edf
KKMinu <58761098+MinuLoL@users.noreply.github.com>
2020-08-03 15:25:45 +0900

    Update 이친수.cpp
    자료값의 범위 생각못함

--- DP/이친수.cpp
+++ DP/이친수.cpp
@@ -1,11 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-int d[90];
-int dp(int x)//추가된 갯수 
+long long d[90];
+long long dp(int x)//추가된 갯수 
 {
+	if(x==0)return 0;
 	if(x==1)return 1;
-	if(x==2)return 1;
-	if(x==3)return 2;
 	if(d[x]!=0)return d[x];
 	else
 	{
