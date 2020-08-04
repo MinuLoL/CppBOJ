b083e8d6a0d26452dc8c070a5522f83547b0f805
MinuLoL <alsdn4837@naver.com>
2020-08-04 17:13:41 +0900

    thinking how to solve

---
+++ DP/포도주시식.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> v;
+
+
+
+int main(){
+	int n;
+	cin>>n;
+	for(int i=0;i<n;++i)
+	{
+		int num;
+		cin>>num;
+		v.push_back(num);
+	}
+}
+
