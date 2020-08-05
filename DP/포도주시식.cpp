aecbefa96a94e50b065a21ff2af05b30c161cc99
KKMinu <58761098+MinuLoL@users.noreply.github.com>
2020-08-05 13:23:41 +0900

    Update 포도주시식.cpp

--- DP/포도주시식.cpp
+++ DP/포도주시식.cpp
@@ -1,17 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> v;
+int v[10001]={0,};
+int d[10001]={0,};
 
-
-
+int dp(int x)
+{
+	if(x<=0)return 0;
+	if(x==1)return v[1];
+	if(x==2)return v[1]+v[2];
+	if(d[x]!=0)return d[x];
+	else
+	{
+		d[x]=max({dp(x-3)+v[x]+v[x-1],dp(x-2)+v[x],dp(x-1)});
+		return d[x];
+	}
+}
+	
 int main(){
 	int n;
 	cin>>n;
-	for(int i=0;i<n;++i)
+	for(int i=1;i<=n;++i)
 	{
-		int num;
-		cin>>num;
-		v.push_back(num);
+		cin>>v[i];
 	}
+	
+	cout<<dp(n);
+	return 0;
 }
-
