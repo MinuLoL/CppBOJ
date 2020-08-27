08823e68f459dcc6160400293bd05ddeb65fd3d1
MinuLoL <alsdn4837@naver.com>
2020-08-27 10:41:05 +0900

    update 유기농배추.cpp

--- DFS/유기농배추.cpp
+++ DFS/유기농배추.cpp
@@ -1,93 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+vector<int> v;
 int main()
 {
 	int T;
 	cin>>T;
-	int M,N,K;
-	int cnt=0;
-	vector<int> cntarr;
-	for(int i=0;i<T;++i)
-	{
-		vector<pair<int,int>> arr; //1이 입력된 좌표
-		cin>>M>>N>>K;
-		vector<vector<int>> v(M,vector<int>(N,0));
 
+	while(T--)
+	{
+		int num=0;
+		int M,N,K;
+		cin>>M>>N>>K;
+		int map[50][50];
+		for(int i=0;i<50;++i)
+			for(int j=0;j<50;++j)
+				map[i][j]=0;
+
+		
 		for(int j=0;j<K;++j)
 		{
 			int x;
 			int y;
 			cin>>x>>y;
-			v[x][y]=1;
+			map[x][y]=1;
 		}
-
-		for(int i=0;i<M;++i)
+		
+		int result=0;
+		
+		while(true)
 		{
-			for(int j=0;j<N;++j)
+			bool find=false;
+			for(int i=0;i<M;++i)
 			{
-				if(v[i][j]==1)
+				for(int j=0;j<N;++j)
 				{
-					arr.push_back(make_pair(i,j)); //1이 입력된 좌표를 차례대로 저장
+					if(map[i][j]==1)
+					{
+						find=true;
+						map[i][j]=2;
+						result++;
+						break;
+					}
 				}
+				if(find==true)
+					break;
 			}
+			
+			if(find==false)
+				break;
+			
+			bool extend;
+			do
+			{
+				extend=false;
+				for(int i=0;i<M;++i)
+					for(int j=0;j<N;++j)
+						if(map[i][j]==2)
+						{
+							if(i>0 && map[i-1][j]==1){ map[i-1][j]=2; extend=true; }
+							if(i<M-1 && map[i+1][j]==1){ map[i+1][j]=2; extend=true; }
+							if(j>0 && map[i][j-1]==1){ map[i][j-1]=2; extend=true; }
+							if(j<N-1 && map[i][j+1]==1){ map[i][j+1]=2; extend=true; }
+						}
+			}
+			while(extend==true);
+			
 		}
-//		
-//		for(int i=0;i<K;++i)
-//		{
-//			cout<<arr[i].first<<" "<<arr[i].second<<endl;
-//		}
-		for(int k=0;k<K;++k)
-		{
-			int x=arr[k].first;
-			int y=arr[k].second;
-			if(x==M-1&&y!=N-1) //맨 아랫줄일때는 오른쪽 칸만 검사
-			{
-				if(v[x][y+1]==1)
-				{
-					v[x][y]=0;
-				 } 
-			}
-			else if(y==N-1&&x!=M-1) //맨 오른쪽줄일때는 아래칸만 검사
-			{
-				if(v[x+1][y]==1)
-				{
-					v[x][y]=0;
-				}
-			}
-			else if(x==M-1&&y==N-1) //맨 마지막칸일때는 검사 안함
-			{}
-			else                     //오른쪽과 아래칸만 검사
-			{
-				if(v[x][y+1]==1||v[x+1][y]==1)
-				{
-					v[x][y]=0;
-				}
-			}
-		}
-		cnt=0;
-		for(int i=0;i<K;++i)    //1이 입력된 좌표를 돌아다니며 1의 갯수 구함
-		{
-			int x=arr[i].first;
-			int y=arr[i].second;
-			if(v[x][y]==1)
-			{
-				cnt++;
-			}
-		}
-		cntarr.push_back(cnt);
-//      for(int i=0;i<M;++i)
-//		{
-//			for(int j=0;j<N;++j)
-//			{
-//				cout<<v[i][j]<<" ";
-//			}
-//			cout<<endl;
-//		}
+		v.push_back(result);
 	}
-	for(int i=0;i<T;++i)
+	for(vector<int>::iterator iter=v.begin();iter<v.end();++iter)
 	{
-		cout<<cntarr[i]<<endl;
+		cout<<*iter<<endl;
 	}
-	
 }
