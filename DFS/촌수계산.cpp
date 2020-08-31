eecb0e6f7dfea79900dd523c7f6040e3d78be972
MinuLoL <alsdn4837@naver.com>
2020-08-31 14:21:19 +0900

    add 촌수계산.cpp

---
+++ DFS/촌수계산.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int visitDFS[100];	
+vector<int> a[101];
+
+void dfs(int start,int end,int count){
+	if(visitDFS[start]){
+		return;
+	}
+	visitDFS[start]=true;
+	if(start==end)
+		cout<<count;
+	for(int i=0;i<a[start].size();++i)
+	{
+		int x=a[start][i];
+		dfs(x,end,++count);
+		count--;
+	}
+
+
+}
+int main()
+{
+	int n;
+	cin>>n;
+
+	int v1,v2;
+	cin>>v1>>v2;
+
+	int m;
+	cin>>m;
+
+	for(int i=0;i<m;++i)
+	{
+		int x;
+		int y;
+		cin>>x>>y;
+		a[x].push_back(y);
+		a[y].push_back(x);		
+	}
+	dfs(v1,v2,0); //7 3
+	if(!visitDFS[v2])
+		cout<<-1;
+	return 0;
+}
