c936c14c1475ab9a5d74dfcb910eeb7e91d61012
MinuLoL <alsdn4837@naver.com>
2020-09-03 11:25:13 +0900

    update 토마토.cpp correct

--- BFS/토마토.cpp
+++ BFS/토마토.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 int M,N;
-int maps[1000][1000];
-int visited[1000][1000];
 queue<pair<int,int> > q;
+
+int maps[1001][1001];
+int visited[1001][1001];
+
 //상하좌우 이동
 int dx[] = { 0,0,-1,1 };
 int dy[] = { 1,-1,0,0 };
@@ -12,22 +14,10 @@ int dy[] = { 1,-1,0,0 };
 bool isInMap(int y, int x){
 	return ((y >= 0 && y < N) && (x >= 0 && x < M));
 } 
-bool com(int a,int b)
-{
-	if(a>b)
-		return true;
-	else
-		return false;
-}
-void bfs(int y,int x){
-	queue<pair<int,int> > q;
-
-	q.push(make_pair(y,x));
-	visited[y][x]=1;
-	
+void bfs(void){
 	while(!q.empty()){
-		y=q.front().first;
-		x=q.front().second;
+		int y=q.front().first;
+		int x=q.front().second;
 		q.pop();
 		for(int i=0;i<4;++i)
 		{
@@ -38,12 +28,6 @@ void bfs(int y,int x){
 				visited[ny][nx]=visited[y][x]+1;           //중요)최단거리 구하는 식
 				q.push(make_pair(ny,nx));
 			}
-			else if(isInMap(ny,nx)&&com(visited[ny][nx],visited[y][x])&&maps[ny][nx]==0)
-			{
-				visited[ny][nx]=visited[y][x]+1;           //중요)최단거리 구하는 식
-				q.push(make_pair(ny,nx));
-			}
-			
 		}
 	}
 }
@@ -56,29 +40,14 @@ int main()
 		for(int j=0;j<M;++j)
 		{
 			cin>>maps[i][j];
-		}
-	}
-	for(int i=0;i<N;++i)
-	{
-		for(int j=0;j<M;++j)
-		{
-			if(!visited[i][j]&&maps[i][j]==1)
+			if(maps[i][j]==1)
 			{
-				bfs(i,j);
+				q.push(make_pair(i,j));
+				visited[i][j]=1;
 			}
 		}
 	}
-
-//	for(int i=0;i<N;++i)
-//	{
-//		for(int j=0;j<M;++j)
-//		{
-//			cout<<visited[i][j]<<" ";
-//		}
-//		cout<<endl;
-//	}
-
-
+	bfs();
 	int maxnum=0;
 	for(int i=0;i<N;++i)
 	{
@@ -94,6 +63,11 @@ int main()
 				maxnum=visited[i][j];
 			}
 		}
+	}
+	if(maxnum==0)
+	{
+		cout<<maxnum;
+		return 0;
 	}
 	cout<<maxnum-1;
 }
