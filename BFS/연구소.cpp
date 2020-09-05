15d122e8b8dc36579432a34388411f5b1dd89be2
MinuLoL <alsdn4837@naver.com>
2020-09-05 23:39:03 +0900

    add  연구소 unsolved:

---
+++ BFS/연구소.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int N,M;
+int maps[8][8];
+
+int main()
+{
+	cin>>N>>M;
+	for(int i=0;i<N;++i)
+	{
+		for(int j=0;j<M;++j)
+		{
+			cin>>maps[i][j];
+		}
+	}
+	
+	
+	
+
+}
