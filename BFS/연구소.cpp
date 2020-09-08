58dc60ee12ee991ac8f1905170e4a8a7810f1a3d
KKMinu <58761098+MinuLoL@users.noreply.github.com>
2020-09-08 10:48:14 +0900

    Update 연구소.cpp

--- BFS/연구소.cpp
+++ BFS/연구소.cpp
@@ -3,20 +3,44 @@ using namespace std;
 
 int N,M;
 int maps[8][8];
+vector<pair<int,int> > v;
+
+vector<pair<int,int> > choice(8);
+int cnt;
+
+void comb(int number,int idx)
+{
+	if(idx>=3)
+	{
+		for(int i=0;i<3;++i)
+			cout<<"("<<choice[i].first<<" "<<choice[i].second<<")"<<" ";
+		cout<<endl;
+		return;
+	}
+	if(number>=cnt)
+		return;
+		
+		choice[idx]=v[number];
+		comb(number+1,idx+1);
+		comb(number+1,idx);
+}
 
 int main()
 {
 	cin>>N>>M;
+	
 	for(int i=0;i<N;++i)
 	{
 		for(int j=0;j<M;++j)
 		{
 			cin>>maps[i][j];
+			if(maps[i][j]==0)
+			{
+				v.push_back(make_pair(i,j));
+				cnt++;
+			}
 		}
 	}
-	//combination(조합)으로 0인 위치마다 pair로 배열에 저장해서 그 갯수만큼 조합(ex) 1,2,3 / 1,2,4 / 1,2,5 .....)이렇게 위치한곳에 1이란 벽을 세워서 2(바이러스) 인곳을 찾아
-	//BFS를 시도해서 바이러스가 최소로 퍼지게하는 수를 구하면 답이 나올듯 하다
-	
-	
-
+	comb(0,0);
+	return 0;
 }
