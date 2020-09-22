c112a19ac59f4f4ac478c4dcaf6854385e8031f8
KKMinu <58761098+MinuLoL@users.noreply.github.com>
2020-09-22 10:46:46 +0900

    Update 퇴사.cpp

--- DP/퇴사.cpp
+++ DP/퇴사.cpp
@@ -1,20 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int N; //1~15
-int Time[16]; //1~5
-int Price[16]; //1~1000
-int d[16]={0,};
-int da[16]={0,};
-bool isInArr(int n,int t)
-{
-	if((n+t-1)>N)
-		return false;
-	else
-		return true;
-}
-
-
+int N;
+int Time[16]={0,};
+int Price[16]={0,};
+int dp[16]={0,};
 
 int main()
 {
@@ -23,67 +13,23 @@ int main()
 	{
 		cin>>Time[i]>>Price[i];
 	}
-	
 	for(int i=N;i>=1;--i)
 	{
-		if(!isInArr(i,Time[i]))
+		int nextpos=i+Time[i];
+		if(nextpos>N+1)
 		{
-			da[i]=da[i+1];
-			continue;
-		}
-
-		int sum=0;
-		
-		if(Time[i]>1)
-		{
-			for(int j=i+1;j<=i+Time[i]-1;++j)
-			{
-				sum+=d[j];
-			}
-		}
-	
-		if(Price[i]>=sum)
-		{
-			d[i]=Price[i];
-			for(int j=i+1;j<=i+Time[i]-1;++j)
-			{
-				d[j]=0;
-			}
-			da[i]=Price[i]+da[i+Time[i]];
+			dp[i]=dp[i+1];
 		}
 		else
 		{
-			da[i]=da[i+1];
-			continue;
+			dp[i]=max(dp[i+1],dp[nextpos]+Price[i]);
 		}
 	}
-	
-	
-//	cout<<"d[i]:";
 //	for(int i=1;i<=N;++i)
 //	{
-//		cout<<d[i]<<" ";
+//		cout<<dp[i]<<" ";
 //	}
-//	cout<<endl;
-//	cout<<"da[i]:";
-//	for(int i=1;i<=N;++i)
-//	{
-//		cout<<da[i]<<" ";
-//	}
-//	cout<<endl;
-	
-	int max=0;
-	for(int i=1;i<=N;++i)
-	{
-		if(max<da[i])
-			max=da[i];
-	}
-	cout<<max;
-//	int sum=0;
-//	for(int i=1;i<=N;++i)
-//	{
-//		sum+=d[i];
-//	}
-//	cout<<sum;
+	cout<<dp[1];
 	return 0;
+	
 }
