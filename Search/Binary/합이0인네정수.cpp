fe285fe350dcf88d4698e5b7f34ee52968fb17e9
KKMinu <58761098+MinuLoL@users.noreply.github.com>
2020-09-29 17:48:19 +0900

    Update 합이0인네정수.cpp

--- Search/Binary/합이0인네정수.cpp
+++ Search/Binary/합이0인네정수.cpp
@@ -1,69 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define MAX 4001
 
-int N;
-int A[4000];
-int B[4000];
-int C[4000];
-int D[4000];
-
-vector<long long> v1;
-vector<long long> v2;
+long long AB[MAX*MAX];
+long long CD[MAX*MAX];
 
 int main()
 {
+	int A[MAX];
+	int B[MAX];
+	int C[MAX];
+	int D[MAX];
+	int N;
 	cin>>N;
 	for(int i=0;i<N;++i)
 	{
 		cin>>A[i]>>B[i]>>C[i]>>D[i];
 	}
-	
+	int idx=0;
 	for(int i=0;i<N;++i)
 	{
 		for(int j=0;j<N;++j)
 		{
-			v1.push_back(A[i]+B[j]);
-			v2.push_back((C[i]+D[j])*(-1));
+			AB[idx]=A[i]+B[j];
+			CD[idx++]=(C[i]+D[j]);
 		}
 	}
-	sort(v1.begin(),v1.end());
-	sort(v2.begin(),v2.end());
-//	for(int i=0;i<v1.size();++i)
-//	{
-//		cout<<v1[i]<<" ";
-//	}
-//	cout<<endl;
-//	for(int i=0;i<v1.size();++i)
-//	{
-//		cout<<v2[i]<<" ";
-//	}
-//	cout<<endl;
-	//ÅõÆ÷ÀÎÅÍ¸¦ È°¿ë
-	int start1=0;
-	int start2=0;
-	int cnt=0;
-	int pos;
-	while(start2<=(N*N)&&start1<=(N*N))
-	{
-		if(v1[start1]>v2[start2])
-		{
-			start2++;
-		}
-		else if(v1[start1]<v2[start2])
-		{
-			start1++;
-		}
-		else
-		{
-			pos=start2;
-			while(v1[start1]==v2[pos])
-			{
-				cnt++;
-				pos++;
-			}
-			start1++;
+	sort(CD,CD+N*N);
 
-		}
-	}
-	cout<<cnt;
+	long long cnt = 0;
+
+    for (int i = 0; i < N * N; i++) {    // ABë°°ì—´ì˜ ì²˜ìŒë¶€í„° N*Në²ˆì§¸ê¹Œì§€ BSë¡œ ìŒì„ ì°¾ëŠ”ë‹¤.
+ 
+		auto range=equal_range(CD,CD+N*N,-AB[i]);
+        cnt += range.second-range.first;
+
+    }
+    cout<<cnt;
+    return 0;
 }
