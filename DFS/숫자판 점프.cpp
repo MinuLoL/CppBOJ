a10f5bd89082988e8ff769f612648a5682ad50ed
MinuLoL <alsdn4837@naver.com>
2020-10-12 14:39:18 +0900

    Update 숫자판 점프.cpp

--- DFS/숫자판 점프.cpp
+++ DFS/숫자판 점프.cpp
@@ -5,35 +5,32 @@ int maps[5][5];
 bool visited[1000000];
 int dx[] = { 0,0,-1,1 };
 int dy[] = { 1,-1,0,0 };
-int cnt=0;
-int sum=0;
+int ans=0;
 bool isInMap(int y,int x)
 {
 	return ((y>=0&&y<5)&&(x>=0&&x<5));
 }
 
-void dfs(int y,int x)
+void dfs(int y,int x,int count,int sum)
 {
-	cnt++;
-	if(cnt==5)
+	if(count==7)
 	{
 		if(!visited[sum])
 		{
 			ans++;
+//			cout<<sum<<endl;
 			visited[sum]=true;
 		}
-		cnt=0;
-		sum=0;
 		return;
 	}
-	sum+=maps[y][x]*pow(10,6-cnt);
+	sum+=maps[y][x]*pow(10,6-count);
 	for(int i=0;i<4;++i)
 	{
 		int ny=y+dy[i];
 		int nx=x+dx[i];
 		if(isInMap(ny,nx))
 		{
-			dfs(ny,nx);
+			dfs(ny,nx,count+1,sum);
 		}
 	}
 }
@@ -48,6 +45,13 @@ int main()
 			cin>>maps[i][j];
 		}
 	}
-	
-
+	for(int i=0;i<5;++i)
+	{
+		for(int j=0;j<5;++j)
+		{
+			dfs(i,j,1,0);
+		}
+	}
+	cout<<ans;
+	return 0;
 }
