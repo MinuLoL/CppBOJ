edfd11ef8ab6caa7c636a59392d2d329e96ad227
MinuLoL <alsdn4837@naver.com>
2020-10-19 10:10:15 +0900

    Update 벽 부수고 이동하기.cpp

--- BFS/벽 부수고 이동하기.cpp
+++ BFS/벽 부수고 이동하기.cpp
@@ -9,6 +9,8 @@ int N,M;
 int maps[1001][1001];
 int visited[1001][1001]={0,};
 int cnt=0;
+vector<pair<int,int> > v;
+int minlen=1000000;
 
 bool isInMap(int y,int x)
 {
@@ -49,13 +51,45 @@ int main()
 			maps[i][j]=str[j]-'0';	
 		}	
 	}
-	bfs(0,0);
-	if(!visited[N-1][M-1])
+	
+	for(int i=0;i<N;++i)	//4방향 중에 이동할 수 있는 0이 하나라도 있는 경우 
+	{
+		for(int j=0;j<M;++j)
+		{
+			if(maps[i][j]==1)
+			{
+				for(int k=0;k<4;++k)
+				{
+					int ny=i+dy[k];
+					int nx=j+dx[k];
+					if(isInMap(ny,nx)&&!maps[ny][nx])
+					{
+						v.push_back(make_pair(i,j));
+						break;
+					}
+				}	
+			}	
+		}	
+	}
+
+	for(int i=0;i<v.size();++i)
+	{
+		int y=v[i].first;
+		int x=v[i].second;
+		maps[y][x]=0;
+		bfs(0,0);
+		if(minlen>visited[N-1][M-1]&&visited[N-1][M-1])
+			minlen=visited[N-1][M-1];
+		memset(visited,0,sizeof(visited));
+		maps[y][x]=1;
+	}
+	
+	if(minlen==1000000)
 	{
 		cout<<-1;
 		return 0;
 	}
-	cout<<visited[N-1][M-1];
+	cout<<minlen;
 	return 0;
 	
 }
