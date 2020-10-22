3ed5a8b8d1d832f1a175f46e57f933e9af3d43be
MinuLoL <alsdn4837@naver.com>
2020-10-22 11:51:44 +0900

    Update 구슬 탈출2.cpp

--- BFS/구슬 탈출2.cpp
+++ BFS/구슬 탈출2.cpp
@@ -3,8 +3,26 @@ using namespace std;
 
 int N,M;
 char maps[11][11];
+int Ry,Rx;
+int By,Bx;
+int Oy,Ox;
+int gravCnt=0;
 
-
+int bfs(int ry,int rx,int by,int bx)
+{
+	queue<pair<pair<pair<int,int>,pair<int,int> >,int> > q;	//(Ry,Rx),(By,Bx)
+	q.push(make_pair(make_pair(make_pair(ry,rx),make_pair((by,bx))),0);
+	
+	while(!q.empty())
+	{
+		
+		
+//		if(ry==Oy&&rx==Ox)
+//		{
+//			return gravCnt;
+//		}
+	}
+}
 
 
 int main()
@@ -13,7 +31,39 @@ int main()
 	for(int i=0;i<N;++i)
 	{
 		cin>>maps[i];
+		for(int j=0;j<M;++j)
+		{
+			if(maps[i][j]=='R')
+			{
+				Ry=i;
+				Rx=j;
+			}
+			else if(maps[i][j]=='B')
+			{
+				By=i;
+				Bx=j;
+			}
+			else if(maps[i][j]=='O')
+			{
+				Oy=i;
+				Ox=j;
+			}
+		}
 	}
-	
+//	cout<<"Rpos:"<<Ry<<" "<<Rx<<endl;
+//	cout<<"Bpos:"<<By<<" "<<Bx<<endl;
+//	cout<<"Opos:"<<Oy<<" "<<Ox<<endl;
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
