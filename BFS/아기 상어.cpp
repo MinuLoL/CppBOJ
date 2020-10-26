a2e6c7585a4a38eebf060335ae58156f29b1f11c
MinuLoL <alsdn4837@naver.com>
2020-10-26 13:18:41 +0900

    Create 아기 상어.cpp

---
+++ BFS/아기 상어.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int N,M;
+int maps[20][20];
+int visited[20][20]; 
+int SharkSize=2;
+int sizecnt=0;
+
+int dy[]={1,0,-1,0};
+int dx[]={0,1,0,-1};
+
+bool isInMap(int y,int x)	//checking in maps
+{
+	return ((y>=0&&y<N)&&(x>=0&&x<N));
+}
+bool possiblepass(int y,int x)	//checking Shark can pass  
+{
+	return	maps[y][x]<SharkSize;
+}
+
+void bfs(int y,int x)
+{
+	queue<pair<int,int> > q;
+	q.push(make_pair(y,x));
+	visited[y][x]=0;
+	while(!q.empty())
+	{
+		y=q.front().first;
+		x=q.front().second;
+		q.pop();
+		for(int i=0;i<4;++i)
+		{
+			int ny=y+dy[i];
+			int nx=x+dx[i];
+			if(isInMap(ny,nx)&&possiblepass(ny,nx))
+			{
+				visited[ny][nx]=visited[y][x]+1;
+				q.push(make_pair(ny,nx));
+			}
+		}
+		
+	}
+}
+
+int main()
+{
+	cin>>N;	//(2<=N<=20)
+	for(int i=0;i<N;++i)
+	{
+		for(int j=0;j<N;++j)
+		{
+			cin>>maps[i][j];
+		}
+	}
+	
+
+}
